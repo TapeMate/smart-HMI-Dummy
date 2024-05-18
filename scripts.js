0cb6f065f28e427cc56d8f4d2b9f1f16d8bb3731
TapeMate <admin@tapemate.onmicrosoft.com>
2024-05-18 21:52:59 +0200

    implemented menu mobile toggle open close.

--- scripts.js
+++ scripts.js
@@ -2,11 +2,24 @@
 
 // eventlistener for active link styling
 const navItems = document.querySelectorAll(".nav-link");
-console.log(navItems);
 
 navItems.forEach((navItem) => {
   navItem.addEventListener("click", (e) => {
     navItems.forEach((item) => item.classList.remove("active"));
     navItem.classList.add("active");
   });
+});
+
+// toggle menu mobile open close
+const toggleIcon = document.querySelector("#menu-mobile");
+const navItemContainer = document.querySelector(".nav-items");
+
+toggleIcon.addEventListener("click", (e) => {
+  if (navItemContainer.classList.contains("close")) {
+    navItemContainer.classList.remove("close");
+    navItemContainer.classList.add("open");
+  } else {
+    navItemContainer.classList.remove("open");
+    navItemContainer.classList.add("close");
+  }
 });
