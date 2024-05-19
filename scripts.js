673142ccfc103bb64548a11f1c65eaa8abad28da
TapeMate <admin@tapemate.onmicrosoft.com>
2024-05-19 12:39:26 +0200

    added class to menu items. added eventlistener add / remove check on media query for collapse expand animation in mobile menu. added functions to handle those events. added helper classes in css.

--- scripts.js
+++ scripts.js
@@ -30,7 +30,67 @@ toggleIcon.addEventListener("click", (e) => {
   }
 });
 
+// make mobile product menu expand and collapse
+const toggleHeader = document.querySelectorAll(".toggle");
+const toggleSubHeader = document.querySelectorAll(".toggle-sub");
+const menuItems = document.querySelectorAll(".menu-item");
+
+// event handler function for main header
+const toggleContent = (e) => {
+  const header = e.currentTarget;
+  let sibling = header.nextElementSibling;
+  while (sibling && sibling.classList.contains("toggle-sub")) {
+    if (sibling.classList.contains("collapse")) {
+      sibling.classList.remove("collapse");
+      sibling.classList.add("expand");
+    } else {
+      sibling.classList.remove("expand");
+      sibling.classList.add("collapse");
+    }
+    sibling = sibling.nextElementSibling;
+  }
+};
+
+// event handler function for sub header
+const toggleSubContent = (e) => {
+  const subHeader = e.currentTarget;
+  const children = subHeader.querySelectorAll("li");
+  children.forEach((child) => {
+    if (child.classList.contains("collapse")) {
+      child.classList.remove("collapse");
+      child.classList.add("expand");
+    } else {
+      child.classList.remove("expand");
+      child.classList.add("collapse");
+    }
+  });
+};
+
+// media query check to add / remove event listener
+const toggleEventListener = () => {
+  if (window.matchMedia("(max-width: 550px)").matches) {
+    toggleHeader.forEach((header) => {
+      header.addEventListener("click", toggleContent);
+    });
+    toggleSubHeader.forEach((subHeader) => {
+      subHeader.addEventListener("click", toggleSubContent);
+    });
+  } else {
+    toggleHeader.forEach((header) => {
+      header.removeEventListener("click", toggleContent);
+    });
+    toggleSubHeader.forEach((subHeader) => {
+      subHeader.removeEventListener("click", toggleSubContent);
+    });
+  }
+};
+
+// initial check to add event listener
+toggleEventListener();
+
 // media query check to prevent navbar items stay hidden when moving back to desktop view
+// also executes the event listener add / remove functino
+// maybe refactor to more generic name and build?
 const mediaQueryCheck = (e) => {
   if (e.matches) {
     navItemContainer.classList.remove("open");
@@ -39,6 +99,8 @@ const mediaQueryCheck = (e) => {
     navItemContainer.classList.add("open");
     navItemContainer.classList.remove("close");
   }
+
+  toggleEventListener();
 };
 
 // media query object and listener
